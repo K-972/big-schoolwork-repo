8045f4fde0de23824554c1d5dc6a970672de6583
Ethan Humphreys <116691290+K-972@users.noreply.github.com>
2023-02-13 14:57:17 +0000

    grade finder

---
+++ algorithmic thinkng/grade setter/main.py
@@ -0,0 +1,34 @@
+
+
+
+def get_values():
+    childs_name = input("Enter the childs name: ")
+    chlids_mark = input("Enter the childs mark: ")
+    chlids_age = input("Enter the childs age: ")
+    return childs_name, chlids_mark, chlids_age
+
+
+def main(childs_name, chlids_mark, chlids_age):
+
+    #############################
+    ###     calculations      ###
+    #############################
+
+    if chlids_mark > 50:
+        childs_grade = "fail"
+    elif chlids_mark <= 50 and chlids_mark < 70:
+        childs_grade = "pass"
+    elif chlids_mark >= 70 and chlids_mark < 90:
+        childs_grade = "merit"
+    elif chlids_mark >= 90:
+        childs_grade = "distinction"
+
+    with open("grades.txt", "a") as f:
+        f.write(childs_name + "," + chlids_mark + "," + chlids_age + "," + childs_grade + "\n")
+
+    
+
+get_values()
+main(*get_values())
+
+    
