3202d773ee4a2be18c0bd461f8435ceaea2a37fa
Ethan Humphreys <116691290+K-972@users.noreply.github.com>
2023-02-16 21:09:06 +0000

    added grade boundries

--- algorithmic thinkng/grade setter/main.py
+++ algorithmic thinkng/grade setter/main.py
@@ -2,45 +2,57 @@ import pandas as pd
 import os
 cwd = os.getcwd()
 
-
 number_of_inputs = int(input("Enter the number of kids to input: "))
 current_kids_done = 0
 max_marks = int(input("Enter the maximum marks: "))
 
 # create an empty DataFrame to hold all the kids' data
-df = pd.DataFrame(columns=['Name', 'Age', 'Mark', 'max marks', 'grade', 'percentage'])
+df = pd.DataFrame(columns=['Name', 'Mark', 'Max Marks', 'Grade', 'Percentage'])
 
 def main():
-    global df  # use the global DataFrame
+    global df
+    # get input from user
+    name = input("Enter the student's name: ")
+    mark = int(input("Enter the student's mark: "))
+    print()
 
-    chlids_name = str(input("Enter the childs name: "))
-    chlids_mark = int(input("Enter the childs mark: "))
-    print("\n")
-
-    #############################
-    ###     calculations      ###
-    #############################
-
-    percentage = (chlids_mark / max_marks) * 100
+    # calculate grade and percentage
+    percentage = (mark / max_marks) * 100
     percentage = round(percentage, 2)
-    
-    if chlids_mark < 50:
-        childs_grade = "fail"
-    elif chlids_mark >= 50 and chlids_mark < 70:
-        childs_grade = "pass"
-    elif chlids_mark >= 70 and chlids_mark < 90:
-        childs_grade = "merit"
-    elif chlids_mark >= 90:
-        childs_grade = "distinction"
 
-    with open("grades.txt", "a") as file:
-        chlids_mark = str(chlids_mark)
-        percentage = str(percentage)
-        file.write(chlids_name + "," + chlids_mark + "," + childs_grade + "," + percentage + "\n")
-    
-    # append the new data to the DataFrame
-    new_data = {'Name': chlids_name, 'Mark': chlids_mark, 'max marks': max_marks, 'grade': childs_grade, 'percentage': percentage}
-    df = df.append(new_data, ignore_index=True)
+    # calculate grade boundries
+
+    # 50%
+    fail_grade_boundries = (max_marks * 0.5)
+    # 70%
+    pass_grade_boundries = (max_marks * 0.7)
+    # 90%
+    merit_grade_boundries = (max_marks * 0.9)
+
+    if mark < fail_grade_boundries:
+        grade = "Fail"
+    elif mark > fail_grade_boundries and mark < pass_grade_boundries:
+        grade = "Pass"
+    elif mark > pass_grade_boundries and mark < merit_grade_boundries:
+        grade = "Merit"
+    elif mark > merit_grade_boundries:
+        grade = "Distinction"
+
+    with open("grades.txt", "a") as f:
+        f.write(f"{name}, {mark}, {max_marks}, {grade}, {percentage}\n")
+
+    # append the new data to the existing DataFrame
+    new_data = pd.DataFrame({"Name": [name],
+                             "Mark": [mark],
+                             "Max Marks": [max_marks],
+                             "Grade": [grade],
+                             "Percentage": [percentage]})
+
+    df = pd.concat([df, new_data], ignore_index=True)
+
+    # write the updated DataFrame to an Excel file
+    with pd.ExcelWriter("output.xlsx") as writer:
+        df.to_excel(writer, sheet_name="Sheet1", index=False)
 
 if __name__ == "__main__":
     for current_kid in range(number_of_inputs):
