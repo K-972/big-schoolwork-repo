b8ecce1188553ad37df82ac0fb56ec9e9fffa65b
Ethan Humphreys <116691290+K-972@users.noreply.github.com>
2023-02-16 20:17:49 +0000

    mad changes - long??|!!

--- algorithmic thinkng/grade setter/main.py
+++ algorithmic thinkng/grade setter/main.py
@@ -2,17 +2,19 @@ import pandas as pd
 import os
 cwd = os.getcwd()
 
-max_marks = 100
 
 number_of_inputs = int(input("Enter the number of kids to input: "))
 current_kids_done = 0
 max_marks = int(input("Enter the maximum marks: "))
 
+# create an empty DataFrame to hold all the kids' data
+df = pd.DataFrame(columns=['Name', 'Age', 'Mark', 'max marks', 'grade', 'percentage'])
+
 def main():
+    global df  # use the global DataFrame
 
     chlids_name = str(input("Enter the childs name: "))
     chlids_mark = int(input("Enter the childs mark: "))
-    chlids_age = input("Enter the childs age: ")
     print("\n")
 
     #############################
@@ -20,10 +22,10 @@ def main():
     #############################
 
     percentage = (chlids_mark / max_marks) * 100
-
+    percentage = round(percentage, 2)
+    
     if chlids_mark < 50:
         childs_grade = "fail"
-        
     elif chlids_mark >= 50 and chlids_mark < 70:
         childs_grade = "pass"
     elif chlids_mark >= 70 and chlids_mark < 90:
@@ -33,27 +35,18 @@ def main():
 
     with open("grades.txt", "a") as file:
         chlids_mark = str(chlids_mark)
-        file.write(chlids_name + "," + chlids_mark + "," + chlids_age + "," + childs_grade + percentage + "\n")
+        percentage = str(percentage)
+        file.write(chlids_name + "," + chlids_mark + "," + childs_grade + "," + percentage + "\n")
     
-
-    # create a sample DataFrame
-    data = {'Name': [chlids_name],
-            'Age': [chlids_age],
-            'Mark': [chlids_mark],
-            'max marks': [max_marks],
-            'grade': [childs_grade],
-            'percentage': [percentage]}
-    df = pd.DataFrame(data)
-
-    # write the DataFrame to an Excel file
-    with pd.ExcelWriter('output.xlsx') as writer:
-        df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-
+    # append the new data to the DataFrame
+    new_data = {'Name': chlids_name, 'Mark': chlids_mark, 'max marks': max_marks, 'grade': childs_grade, 'percentage': percentage}
+    df = df.append(new_data, ignore_index=True)
 
 if __name__ == "__main__":
     for current_kid in range(number_of_inputs):
         main()
         current_kids_done += 1
 
-    
+    # write the complete DataFrame to the Excel file
+    with pd.ExcelWriter('output.xlsx') as writer:
+        df.to_excel(writer, sheet_name='Sheet1', index=False)
