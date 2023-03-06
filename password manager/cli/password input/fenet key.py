dea93d3a769960aea95315260c75daa6ac3b5e29
Ethan Humphreys <116691290+K-972@users.noreply.github.com>
2023-03-06 14:55:14 +0000

    done more and key made

---
+++ password manager/cli/password input/fenet key.py
@@ -0,0 +1,2 @@
+key = 'bgWh7lXLgHu8htFRKI7VCkTH8IdMn_l_1O1ZIcm5mCk='
+print('This is your key so keep it safe: bgWh7lXLgHu8htFRKI7VCkTH8IdMn_l_1O1ZIcm5mCk=')
--- password manager/cli/password input/main.py
+++ password manager/cli/password input/main.py
@@ -2,11 +2,13 @@ import hashlib
 import time
 #from hashlib import *
 
+
+
 print('welcome to my password manager')
 time.sleep(1)
 
 def main_menu_no_login():
-    decicion = input('Enter: 1 - to log in, 2 - to quit and 3 - to to be called tony')
+    decicion = int(input('Enter: 1 - to log in, 2 - to quit and 3 - to to be called tony: '))
     if decicion == 1:
         log_in()
     elif decicion == 2:
@@ -37,12 +39,12 @@ def add_password():
     new_password = hashlib.sha256(new_password.encode('utf-8')).hexdigest()
 
     array.append([new_username, new_password])
+    print(array)
 
 
                         
 
 def log_in():
-    print("Please log in")
 
     # Enter user and password and create hash
     entered_user = input("Enter username: ")
@@ -52,16 +54,30 @@ def log_in():
 
     # Check if username and password match with the elements of the array
     for i in range(len(array)):
-        if entered_user_hash == hashlib.sha256(array[i][0].encode('utf-8')).hexdigest():
-            if entered_password_hash == hashlib.sha256(array[i][1].encode('utf-8')).hexdigest():
-                print("Login successful")
-                pass
-            else:
-                print("Incorrect password")
-                pass
+        if entered_user_hash == hashlib.sha256(array[i][0].encode('utf-8')).hexdigest() and entered_password_hash == hashlib.sha256(array[i][1].encode('utf-8')).hexdigest():
+            print("Login successful")
+            main_menu_login()
+        else:
+            print("Incorrect password\n")
+            time.sleep(1)
+            main_menu_no_login()
 
 
-log_in()
+
 
 def main_menu_login():
-    
+    option = int(input('Enter: 1 - view passwords, 2 - add passwords, 3 - remove passwords, 4 - logout: '))
+    if option == 1:
+        pass
+    if option == 2:
+        add_password()
+    if option == 3:
+        pass
+    if option  == 4:
+        print("Log out successful")
+        time.sleep(1)
+        main_menu_no_login()
+
+
+# starting functions
+main_menu_no_login()
