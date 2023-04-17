233492d5339aca69c748efb4f8715ab233dc7475
Ethan Humphreys <116691290+K-972@users.noreply.github.com>
2023-04-17 13:50:18 +0000

    made stuff

--- algorithmic thinkng/grade setter/main.py
+++ algorithmic thinkng/grade setter/main.py
@@ -63,6 +63,6 @@ if __name__ == "__main__":
     for current_kid in range(number_of_inputs):
         main()
 
-    # write the complete DataFrame to an Excel file namwd output
+    # write the complete DataFrame to an Excel file named output
     with pd.ExcelWriter('output.xlsx') as writer:
         df.to_excel(writer, sheet_name='Sheet1', index=False)
---
+++ algorithmic thinkng/python challenges rip/hackproof/main.py
@@ -0,0 +1,45 @@
+import hashlib
+from hashlib import sha256
+
+# i don't know what this does or why it is here. but it stays because everytime i remove it my programme breaks
+import os
+print(os.getcwd())
+
+def create_password():
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+    username = hashlib.sha256(username.encode('utf-8')).hexdigest()
+    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    with open('password.txt', 'a', encoding='utf-8') as file:
+        file.write(username)
+        file.write('\n')
+        file.write(password)
+        file.write('\n')
+
+def login():
+    alive_username = input('Enter your username: ')
+    alive_password = input('Enter your password: ')
+    alive_username = hashlib.sha256(alive_username.encode('utf-8')).hexdigest()
+    alive_password = hashlib.sha256(alive_password.encode('utf-8')).hexdigest()
+
+    with open('password.txt', 'r') as file:
+        line = file.readlines()
+        username = line[1]
+        password = line[2]
+
+    if alive_password == password and alive_username == username:
+        with open('document.txt', 'r', encoding='utf-8') as file:
+            print('to do')
+            pass
+
+
+
+
+
+main_menu = input('press - 1 to login    press - 2 to create a password: ')
+if main_menu == '1':
+    login()
+elif main_menu == '2':
+    create_password()
+else:
+    pass
