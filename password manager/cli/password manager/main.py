0f7bd8c940ea8f7052649583a4c26df24461404b
Ethan Humphreys <116691290+K-972@users.noreply.github.com>
2023-09-29 12:44:43 +0000

    hi

--- password manager/cli/password manager/main.py
+++ password manager/cli/password manager/main.py
@@ -5,9 +5,7 @@ from cryptography.fernet import Fernet
 
 
 
-password = 'e5dfed079b2bb64718bb391046739a3e342145baa274d5290f4099566531c4ee'
 
-hashed_key = 'c6b34ed2e923b8670ff6f4c675f9e2c71565a5b7c88384d422e46c404652a5a3'
 
 def edit():
     true_statement_1 = True
@@ -61,6 +59,9 @@ def main():
     password_input = hashlib.sha256(str.encode(password_input)).hexdigest()
     key = input("Enter encryption key: ")
     key_hashed = 'variable assignment'
+    password = 'e5dfed079b2bb64718bb391046739a3e342145baa274d5290f4099566531c4ee'
+
+    hashed_key = 'c6b34ed2e923b8670ff6f4c675f9e2c71565a5b7c88384d422e46c404652a5a3'
     key_hashed = hashlib.sha256(str.encode(key_hashed)).hexdigest()
     if password_input == password and key_hashed == hashed_key:
         allowed_access = True
@@ -92,7 +93,7 @@ def main():
 
     else:
         allowed_access = False
-        print("wrong password you sussy imposter.")
+        print("wrong password")
         time.sleep(5)
         sys.exit()
 
